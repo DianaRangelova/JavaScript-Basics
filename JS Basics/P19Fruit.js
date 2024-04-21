b874f40c1131e62e2701cc3b547342c7f138e8a5
DianaRangelova <diana_kirilova@mail.bg>
2024-04-21 16:19:43 +0300

    JS Basics

---
+++ JS Basics/P19Fruit.js
@@ -0,0 +1,7 @@
+//Declare Function
+function fruit(fruit, grams, price_per_kg) {
+    let price = (grams / 1000) * price_per_kg
+    console.log(`I need $${price.toFixed(2)} to buy ${(grams / 1000).toFixed(2)} kilograms ${fruit}.`);
+}
+
+fruit('orange', 2500, 1.80);
